f0504c2e3db9ce3f5ea4c7609946e34df9cd654c
Leandro Alexandrino <alexandrinowd@gmail.com>
2017-11-07 23:53:46 -0200

    refactoring returns to json

--- src/album.js
+++ src/album.js
@@ -1,15 +1,16 @@
 import { API_URL } from './config';
+import { toJSON } from './utils';
 
 const getAlbum = id =>
   fetch(`${API_URL}/albums/${id}`)
-    .then(data => data.json());
+    .then(toJSON);
 
 const getAlbums = ids =>
   fetch(`${API_URL}/albums/?ids=${ids}`)
-    .then(data => data.json());
+    .then(toJSON);
 
 const getAlbumTracks = album =>
   fetch(`${API_URL}/albums/${album}/tracks`)
-    .then(data => data.json());
+    .then(toJSON);
 
 export { getAlbum, getAlbums, getAlbumTracks };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { API_URL } from './config';
+import { toJSON } from './utils';
 
 const search = (query, type) => {
   fetch(`${API_URL}/search?q=${query}&type=${type}`)
-    .then(data => data.json());
+    .then(toJSON);
 };
 const searchAlbums = (query) => {
   search(query, 'album');
