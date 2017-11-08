b9835883885cd0a6b922a6b258ecc8fb7f1edc6e
Leandro Alexandrino <alexandrinowd@gmail.com>
2017-11-07 22:49:22 -0200

    added metho to get many albums

--- src/album.js
+++ src/album.js
@@ -3,6 +3,11 @@ const getAlbum = (id) => {
     .then(data => data.json());
 };
 
+const getAlbums = (id) => {
+  fetch(`https://api.spotify.com/v1/albums/?ids=${id}`)
+    .then(data => data.json());
+};
+
 const getAlbumTracks = () => {};
 
-export { getAlbum, getAlbumTracks };
+export { getAlbum, getAlbums, getAlbumTracks };
--- tests/album.spec.js
+++ tests/album.spec.js
@@ -3,7 +3,7 @@
  */
 
 import chai, { expect } from 'chai';
-import { getAlbum, getAlbumTracks } from '../src/album';
+import { getAlbum, getAlbums, getAlbumTracks } from '../src/album';
 import sinon from 'sinon';
 import sinonChai from 'sinon-chai';
 import sinonStubPromise from 'sinon-stub-promise';
@@ -31,6 +31,10 @@ describe('Album', () => {
       expect(getAlbum).to.exist;
     });
 
+    it('should have getAlbums method', () => {
+      expect(getAlbums).to.exist;
+    });
+
     it('should have getAlbumTracks method', () => {
       expect(getAlbumTracks).to.exist;
     });
@@ -56,6 +60,29 @@ describe('Album', () => {
         promise.resolves({ album: 'name' });
         const album = getAlbum('0sNOF9WDwhWunNAHPD3Baj');
         expect(album.resolveValue).to.be.eql({ album: 'name' });
+      });
+  });
+
+  describe('getAlbums', () => {
+    it('should call fetch method', () => {
+    const album = getAlbums();
+    expect(stubedFetch).to.have.been.calledOnce;
+    });
+
+    it('should call fetch with the correct URL', () => {
+      const albums = getAlbums(['41MnTivkwTO3UUJ8DrqEJJ', '6JWc4iAiJ9FjyK0B59ABb4']);
+      expect(stubedFetch).to.have.been.
+        calledWith('https://api.spotify.com/v1/albums/?ids=41MnTivkwTO3UUJ8DrqEJJ,6JWc4iAiJ9FjyK0B59ABb4');
+        
+        const otherAlbums = getAlbums(['41MnTivkwTO3UUJ8DrqEJJ', '6JWc4iAiJ9FjyK0B59ABb4', '6UXCm6bOO4gFlDQZV5yL37'])
+        expect(stubedFetch).to.have.been.
+          calledWith('https://api.spotify.com/v1/albums/?ids=41MnTivkwTO3UUJ8DrqEJJ,6JWc4iAiJ9FjyK0B59ABb4,6UXCm6bOO4gFlDQZV5yL37');
+      });
+
+      it('should return the correct data from promise', () => {
+        promise.resolves({ album: 'name' });
+        const albums = getAlbums(['41MnTivkwTO3UUJ8DrqEJJ', '6JWc4iAiJ9FjyK0B59ABb4']);
+        expect(albums.resolveValue).to.be.eql({ album: 'name' });
       });
   });
 });
