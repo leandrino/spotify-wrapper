6295874e08902216748c8b14e0a16e7f8b68cb6d
Leandro Alexandrino <alexandrinowd@gmail.com>
2017-11-09 00:57:36 -0200

    added headers on methods

--- src/album.js
+++ src/album.js
@@ -1,16 +1,16 @@
-import { API_URL } from './config';
+import { API_URL, HEADERS } from './config';
 import { toJSON } from './utils';
 
 const getAlbum = id =>
-  fetch(`${API_URL}/albums/${id}`)
+  fetch(`${API_URL}/albums/${id}`, HEADERS)
     .then(toJSON);
 
 const getAlbums = ids =>
-  fetch(`${API_URL}/albums/?ids=${ids}`)
+  fetch(`${API_URL}/albums/?ids=${ids}`, HEADERS)
     .then(toJSON);
 
 const getAlbumTracks = album =>
-  fetch(`${API_URL}/albums/${album}/tracks`)
+  fetch(`${API_URL}/albums/${album}/tracks`, HEADERS)
     .then(toJSON);
 
 export { getAlbum, getAlbums, getAlbumTracks };
--- src/config.js
+++ src/config.js
@@ -1,1 +1,7 @@
+const API_TOKEN = 'BQBql_dl6N9sTzsNPbcbDwgRmndZOS32Mrr1LwJse0TUNCbrFTCafZuqSRTI_wilmIJ-RwNYa52kL0yzsBSKwcR1vmAAVWxeJ9ytB4ZSGNvG3hW0lP38xgxaFyF9MrZqtJZipy7Ilz7shhyy';
 export const API_URL = 'https://api.spotify.com/v1';
+export const HEADERS = {
+  headers: {
+    Authorization: `'Bearer ${API_TOKEN}'`,
+  },
+};
