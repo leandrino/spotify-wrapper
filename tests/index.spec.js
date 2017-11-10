7a28411b8020832d37385bfb105b892bde2860dc
Leandro Alexandrino <alexandrinowd@gmail.com>
2017-11-09 22:28:16 -0200

    create request method

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,6 +1,14 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+import sinonStubPromise from 'sinon-stub-promise';
+
+chai.use(sinonChai);
+sinonStubPromise(sinon);
 
 import SpotifyWrapper from '../src/index';
+
+global.fetch = require('node-fetch')
 
 describe('SpotifyWrapper Library', () => {
   it('should create an instance of SpotifyWrapper', () => {
@@ -26,7 +34,7 @@ describe('SpotifyWrapper Library', () => {
   it('should receive token as an option', () => {
     let spotify = new SpotifyWrapper({
       token: 'e078cb80a315c1545d5396567810bf94dc360f30bfdaae14ca6aad6cf9fe768d'
-    })
+    });
 
     expect(spotify.token).to.be.deep.equal('e078cb80a315c1545d5396567810bf94dc360f30bfdaae14ca6aad6cf9fe768d');
   });
@@ -36,6 +44,54 @@ describe('SpotifyWrapper Library', () => {
       token: '9d9280b03dd43a776628d19beebf0e7795d71ee52c7a68e08da76ef32f761482'
     });
 
-    expect(spotify.token).to.be.deep.equal('9d9280b03dd43a776628d19beebf0e7795d71ee52c7a68e08da76ef32f761482');
+    expect(spotify.token).to.be.deep.equal('9d9280b03dd43a776628d19beebf0e7795d71ee52c7a68e08da76ef32f761482')
+  });
+
+  describe('request method', () => {
+    let stubedFetch;
+    let promise;
+
+    beforeEach(() => {
+      stubedFetch = sinon.stub(global, 'fetch');
+    });
+
+    afterEach(() => {
+      stubedFetch.restore();
+    });
+
+    it('should have request method', () => {
+      let spotify = new SpotifyWrapper({});
+
+      expect(spotify.request).to.exist;
+    });
+
+    it('should call fetch when request', () => {
+      let spotify = new SpotifyWrapper({})
+
+      spotify.request('url');
+      expect(stubedFetch).to.have.been.calledOnce;
+    });
+
+    it('should call fetch with the right url passed', () => {
+      let spotify = new SpotifyWrapper({})
+
+      spotify.request('url');
+      expect(stubedFetch).to.have.been.calledWith('url');
+    });
+
+    it('should call fetch with the right url passed', () => {
+      let spotify = new SpotifyWrapper({
+        token: '9d9280b03dd43a776628d19beebf0e7795d71ee52c7a68e08da76ef32f761482'
+      })
+
+      const headers = {
+        headers: {
+          Authorization: 'Bearer 9d9280b03dd43a776628d19beebf0e7795d71ee52c7a68e08da76ef32f761482'
+        }
+      }
+
+      spotify.request('url');
+      expect(stubedFetch).to.have.been.calledWith('url', headers);
+    });
   });
 });
